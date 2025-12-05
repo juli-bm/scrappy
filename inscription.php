9a890a1a8880d89dedc4f98450d9d07d97f5eebc
Julix13 <mouillacjuliette@gmail.com>
2025-12-05 16:26:56 +0100

    1 er test form

---
+++ inscription.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Inscription</title>
+</head>
+<body>
+
+<h2>Formulaire d'inscription</h2>
+
+<form method="post" action="https://azrael.sha.univ-poitiers.fr/~ydelmas/exemples/info-max.php">
+    <label>Nom :</label><br>
+    <input type="text" name="nom"><br><br>
+
+    <label>Email :</label><br>
+    <input type="email" name="email"><br><br>
+
+    <label>Mot de passe :</label><br>
+    <input type="password" name="password"><br><br>
+
+    <button type="submit">S'inscrire</button>
+</form>
+
+</body>
+</html>
